46baae46470032f75cde34450d54a074e026b3f8
SyedaBareeha <bareehafatimaaa@gmail.com>
2025-08-12 14:54:08 +0500

    Add files via upload

---
+++ toy/toymain.py
@@ -0,0 +1,7 @@
+from toy import CuddlyToy,Teddy,Bunny,EngineDriver,Gardener,Clown,BankManager
+def main ():
+    toy1= EngineDriver("teddy","5.1","Blue","Engine driver","10m/s")
+    toy1.describe()
+    toy1.show()
+    toy1.make_noise()
+main()    
