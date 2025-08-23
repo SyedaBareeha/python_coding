a1ef4b0325aae4e665ed94b24d8019e48675a82e
SyedaBareeha <bareehafatimaaa@gmail.com>
2025-08-23 05:52:15 +0500

    Add files via upload

---
+++ geometry/main.py
@@ -0,0 +1,13 @@
+from rational import Rational
+def main():
+    r1=Rational(10,6)
+    r2=Rational(18,12)
+    ans= r1+r2
+    print (ans)
+    
+    r1=Rational(10,6)
+    r2=Rational(18,12)
+    ans= r1/r2
+    print (ans)
+    
+main()      
